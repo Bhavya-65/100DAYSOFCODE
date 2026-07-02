d8afb656854ac3aa5d46d8994bdb3b88c1e6db22
Bhavya Agarwal <bhavya2009206@gmail.com>
2026-07-02 21:27:38 +0530

    Day 6 Solutions

---
+++ Day6/RemoveDuplicates.java
@@ -0,0 +1,41 @@
+package Day6;
+
+import java.util.Scanner;
+
+public class RemoveDuplicates {
+    
+    static void removeDuplicates(int[] arr) {
+
+        if (arr.length == 0) {
+            return;
+        }
+
+        int j = 0;
+
+        for (int i = 1; i<arr.length; i++){
+            if (arr[i] != arr[j]){
+                j++;
+                arr[j] = arr[i];
+            }
+        }
+
+        for (int i = 0; i<=j; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        removeDuplicates(arr);
+
+        sc.close();
+    }
+}
