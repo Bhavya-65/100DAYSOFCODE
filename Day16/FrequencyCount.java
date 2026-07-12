d1f8f55fad024af782e111e994f9ebd976d5db71
Bhavya Agarwal <bhavya2009206@gmail.com>
2026-07-12 22:58:04 +0530

    Day 16 Solutions

---
+++ Day16/FrequencyCount.java
@@ -0,0 +1,49 @@
+package Day16;
+import java.util.*;
+
+public class FrequencyCount {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+
+        // Counting frequency
+        for(int i = 0; i < n; i++){
+
+            if(map.containsKey(arr[i])){
+
+                map.put(arr[i], map.get(arr[i]) + 1);
+
+            }
+            else{
+
+                map.put(arr[i], 1);
+
+            }
+        }
+
+
+        // Printing result
+        for(int key : map.keySet()){
+
+            System.out.print(key + ":" + map.get(key) + " ");
+
+        }
+
+
+        sc.close();
+    }
+}
